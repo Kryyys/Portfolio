bf03e48be8bddc3b7fc5375834292771994e6a54
Marine <mariine.becker@gmail.com>
2023-01-23 20:51:41 +0100

    home content et media queries

--- common/navbar.php
+++ common/navbar.php
@@ -1,15 +1,18 @@
 <nav>
 
     <div id="logo">
-        <img src="images/MB2.png" alt="" width="80px">
+        <a href="home.php?page=home"><img src="images/MB2.png" alt="" width="80px"></a>
         <span id="titre_logo"><a href="home.php?page=home">Marine BECKER</a></span>
     </div>
 
     <div id="onglet">
-
-        <span><a class="liens_nav" href="home.php?page=cv">CV</a></span>
-        <span><a class="lians_nav" href="home.php?page=realisations">Réalisations</a></span>
-        <span><a class="liens_nav" href="home.php?page=veilles">Veilles</a></span>
+    <input type="checkbox" id="toggle" name="hidden">
+    <label for="toggle"><img src="./images/burger_icon.png" alt=""></label>
+        <div class="burger">
+            <span><a class="liens_nav" href="home.php?page=cv">CV</a></span>
+            <span><a class="lians_nav" href="home.php?page=realisations">Réalisations</a></span>
+            <span><a class="liens_nav" href="home.php?page=veilles">Veilles</a></span>
+        </div>
 
     </div>
 
--- content/home_content.php
+++ content/home_content.php
@@ -1,51 +1,54 @@
-<div>
-    <p>Je m'appelle BECKER Marine. Je suis actuellement en formation, dans le but d'obtenir mon titre professionnel de Développeur Web / Web Mobile.
-    </p>
-    <br>
-    <p>Ce site a pour but de vous montrer mes réalisations faites lors de cette formation, ainsi que mes veilles et mon curiculum vitae.</p>
-    <br>
-</div>
-
-<div id="home_cards">
-    <aside class="card">
-        <div id="image_boot">
-
-        </div>
-        <a href="home.php?page=bootstrap" class="bandeau_bas">
-            <div class="centre">
-                <h3>CV</h3>
-                <p>Mon Curiculum Vitae</p>
-                <p>Bootstrap est un framework de développement front-end gratuit et open-source qui permet de créer des sites web réactifs et adaptés aux mobiles. </p>
-                <p>Il contient une collection de composants CSS et JavaScript tels que des barres de navigation, des formulaires, des boutons et des modals que l'on peut facilement intégrer dans un site web pour un design professionnel et cohérent.</p>
+<main>
+    <div id="home_text">
+        <p>Je m'appelle BECKER Marine. Je suis actuellement en formation, dans le but d'obtenir mon titre professionnel de Développeur Web / Web Mobile.
+        </p>
+        <br>
+        <p>Ce site a pour but de vous montrer mes réalisations faites lors de cette formation, ainsi que mes veilles et mon curiculum vitae.</p>
+        <br>
+    </div>
+    
+    <div id="home_cards">
+        <aside class="card">
+            <div id="image_CV">
+    
             </div>
-        </a>
-    </aside>
-
-    <aside class="card">
-        <div id="image_boot">
-
-        </div>
-        <a href="home.php?page=bootstrap" class="bandeau_bas">
-            <div class="centre">
-                <h3>Réalisations</h3>
-                <p>Mes Réalisations</p>
-                <p>Bootstrap est un framework de développement front-end gratuit et open-source qui permet de créer des sites web réactifs et adaptés aux mobiles. </p>
-                <p>Il contient une collection de composants CSS et JavaScript tels que des barres de navigation, des formulaires, des boutons et des modals que l'on peut facilement intégrer dans un site web pour un design professionnel et cohérent.</p>
+            <a href="home.php?page=cv" class="bandeau_bas">
+                <div class="centre">
+                    <h3>CV</h3>
+                    <p>Mon Curiculum Vitae</p>
+                    <p>Envie de connaître mon parcours, de quelle formation je suis issue et quelles sont expériences professionnelles ?</p>
+                    <p>Mon Curiculum Vitae est par ici !</p>
+                </div>
+            </a>
+        </aside>
+    
+        <aside class="card">
+            <div id="image_real">
+    
             </div>
-        </a>
-    </aside>
-
-    <aside class="card">
-        <div id="image_boot">
-
-        </div>
-        <a href="home.php?page=bootstrap" class="bandeau_bas">
-            <div class="centre">
-                <h3>Veilles Technologiques</h3>
-                <p>Mes veille technologique</p>
-                <p>Bootstrap est un framework de développement front-end gratuit et open-source qui permet de créer des sites web réactifs et adaptés aux mobiles. </p>
-                <p>Il contient une collection de composants CSS et JavaScript tels que des barres de navigation, des formulaires, des boutons et des modals que l'on peut facilement intégrer dans un site web pour un design professionnel et cohérent.</p>
+            <a href="home.php?page=realisations" class="bandeau_bas">
+                <div class="centre">
+                    <h3>Réalisations</h3>
+                    <p>Mes Réalisations</p>
+                    <p>J'ai eu l'occasion de réaliser de nombreux projets durant ma formation, chacun validant une ou plusieurs compétences requises pour l'obatention du titre professionnel.</p>
+                    <p>Vous trouverez ici les liens vers ces fameux projets. N'hésitez pas à y jeter un oeil.</p>
+                </div>
+            </a>
+        </aside>
+    
+        <aside class="card">
+            <div id="image_boot">
+    
             </div>
-        </a>
-    </aside>
-</div>
+            <a href="home.php?page=veilles" class="bandeau_bas">
+                <div class="centre">
+                    <h3>Veilles Technologiques</h3>
+                    <p>Mes veilles technologiques</p>
+                    <p>Afin d'apprendre, il m'a fallu faire de nombreuses veilles technologiques sur de nombreuses technologies et logiciels.</p>
+                    <p>Vous retrouverez ici toutes les veilles que j'aie pu rédiger durant ma formation.</p>
+                </div>
+            </a>
+        </aside>
+    </div>
+    <br><br>
+</main>
